33c2c59bc7499c05b942a096d0ceee216bd3e900
alisari41 <alisari41@outlook.com>
2022-03-04 01:10:07 +0300

    Startup.cs ve Program.cs deki operasyonlarımı ekledim.

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -11,6 +11,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Core.DependencyResolves;
+using Core.Extensions;
+using Core.Utilities.IoC;
+using Core.Utilities.Security.Encyption;
+using Core.Utilities.Security.Jwt;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 namespace WebAPI
 {
@@ -23,18 +30,49 @@ namespace WebAPI
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
+            //Bizim API mize izin verdiðim yerin dýþýnda istek gelmesi problemi için güvenlik
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowOrigin",
+                    builder => builder.WithOrigins("http://localhost:3000"));//girdiðim siteden talet gelirse buna izin ver demek
+            });
+
+
+            var tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();//Bizim kendi 
+
+
+            //token için 
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters//configuresyon bilgilerini verriyorum
+                    {
+                        ValidateIssuer = true,//Bu Adama tok verdiðimiz zaten appsetting.jsonda "www.alisari.com" veridðim bilgiyi veriyorum
+                        ValidateAudience = true,
+                        ValidateLifetime = true,//Verdiðim Yaþam süresini kortrol et
+                        ValidIssuer = tokenOptions.Issuer,
+                        ValidAudience = tokenOptions.Audience,
+                        ValidateIssuerSigningKey = true,//Anahtarýda kotrol edeyim mi
+                        IssuerSigningKey = SecurityKeyHelper.CreateSecurityKey(tokenOptions.SecurityKey)
+                    };
+                });
+
+            //Kendi oluþturduðum services
+            services.AddDependencyResolvers(new ICoreModule[]
+            {
+                new CoreModule(),//Benim baþka CoreModule olursa orada ekleyebilirim 
+            });
+
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPI", Version = "v1" });
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -44,11 +82,23 @@ namespace WebAPI
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPI v1"));
             }
 
+            //Kendi eklediðim app. larý tek seferde buradan sýnýfý ekleyerek oluþturuyorum
+            app.ConfigureCustomExceptionMiddleware();
+
+            //Cors(günvenlik) Ýçin Kullandým sýralamasý önemli
+            app.UseCors(builder => builder.WithOrigins("http://localhost:3000").AllowAnyHeader());//Burdan gelen bütün isteklere izin ver
+
+
             app.UseHttpsRedirection();
 
             app.UseRouting();
 
-            app.UseAuthorization();
+            //Sýralamalarý önemli giriþ yapmadan iþlem yapýlmaz gibi düþün
+            //Eklenmesi gerekir
+            app.UseAuthentication();//Giriþ izni gibi düþün giriþ yapabilir
+
+            //Bu yoksa bunuda ekle
+            app.UseAuthorization();//Yetkidir ne yapabilir.
 
             app.UseEndpoints(endpoints =>
             {
